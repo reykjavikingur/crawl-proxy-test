554a752070733af204e78437dcd5bf6fb224d49c
Andrew Strader <astrader@resource.com>
2016-03-30 10:17:06 -0400

    refactor rules to main script

--- proxy.js
+++ proxy.js
@@ -1,9 +1,28 @@
 var startProxyServer = require('./lib/start-proxy-server');
 
-console.log('starting the PROXY server');
-
 startProxyServer({
 	port: 4800,
-	//proxyTarget: 'http://localhost:3600'
-	proxyTarget: 'http://www.postalmethods.com'
+	proxyTarget: 'http://www.postalmethods.com',
+	rules: [
+		//*
+		{
+			filter: function(req) {
+				return req.url === '/';
+			},
+			transform: function(html) {
+				return html.replace('PostalMethods Web-to-Post Service', 'PostalMethods AWESOME Web-to-Post Service');
+			}
+		},
+		//*/
+		//*
+		{
+			filter: function(req) {
+				return req.url === '/faq';
+			},
+			transform: function(html) {
+				return html.replace('What can I use PostalMethods for', 'What AMAZING THINGS can I use PostalMethods for');
+			}
+		}
+		//*/
+	]
 });
